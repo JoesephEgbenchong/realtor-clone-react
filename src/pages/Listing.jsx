e747456e73da69b08ad26bf2c58e0e6f5993b1f9
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2023-03-31 23:20:28 +0100

    Add the listing information to the listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,7 +6,7 @@ import { db } from '../firebase';
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, {EffectFade, Autoplay, Navigation, Pagination} from "swiper";
 import "swiper/css/bundle";
-import { FaShare } from 'react-icons/fa';
+import { FaBath, FaBed, FaChair, FaMapMarkerAlt, FaParking, FaShare } from 'react-icons/fa';
 
 export default function Listing() {
 
@@ -58,6 +58,55 @@ export default function Listing() {
         </div>
         {sharedLinkCopied && (<p className="fixed top-[23%] right-[5%] font-semibold 
         border-2 border-gray-400 rounded-md bg-white z-10 p-2">Link copied</p>)}
+
+        <div className="flex flex-col md:flex-row max-w-6xl lg:mx-auto m-4 p-4 rounded-lg shadow-lg bg-white
+         lg:space-x-5">
+            <div className=" w-full h-[200px] lg:h-[400px]">
+                <p className="text-2xl font-bold text-blue-900">
+                    {listing.name} - ${listing.offer ? listing.discountedPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : 
+                    listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                </p>
+                <p className="flex items-center mt-6 mb-3 font-semibold">
+                    <FaMapMarkerAlt className="text-green-700 mr-1" />{listing.address}
+                </p>
+                <div className="flex justify-start items-center space-x-4 w-[75%]">
+                    <p className="bg-red-800 w-full max-w-[200px] rounded-md p-1 text-white 
+                     text-center font-semibold shadow-md">
+                        {listing.type === "rent" ? "On Rent" : "For Sale"}
+                    </p>
+                    {listing.offer && (
+                    <p className="w-full max-w-[200px] bg-green-800 rounded-md shadow-md text-white font-semibold
+                     p-1 text-center">
+                        ${(listing.regularPrice - listing.discountedPrice).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} discount
+                    </p>)}
+                </div>
+                <p className="mt-3 mb-3">
+                    <span className="font-semibold">Description -</span>
+                    {listing.description}
+                </p>
+                <ul className='flex items-center space-x-2 lg:space-x-10 text-sm font-semibold'>
+                    <li className="flex items-center whitespace-nowrap">
+                        <FaBed className="text-lg mr-3"/>
+                        {+listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : `${listing.bedrooms} Bedroom`}
+                    </li>
+                    <li className="flex items-center whitespace-nowrap">
+                        <FaBath className="text-lg mr-3"/>
+                        {+listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : `${listing.bathrooms} Bathroom`}
+                    </li>
+                    <li className="flex items-center whitespace-nowrap">
+                        <FaParking className="text-lg mr-3"/>
+                        {listing.parking ? "Parking Spot" : "No Parking"}
+                    </li>
+                    <li className="flex items-center whitespace-nowrap">
+                        <FaChair className="text-lg mr-3"/>
+                        {listing.furniture ? "Furnished" : "Not Furnished"}
+                    </li>
+                </ul>
+            </div>
+            <div className="bg-blue-300 w-full h-[200px] lg:h-[400px]">
+
+            </div>
+        </div>
     </main>
   )
 }
